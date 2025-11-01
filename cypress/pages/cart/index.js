349fa7631cdd29cbfd0acb8addc069d52e48e834
brad <brad@cesar.org.br>
2025-11-01 17:16:05 -0300

    Inclusão do TC15

---
+++ cypress/pages/cart/index.js
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker'
+
+export const SUCCESS_MSG_ORDER_PLACED ='Congratulations! Your order has been confirmed!'
+
+class Cart {
+    addProductCart = (productId) => {
+        cy.get(`a[data-product-id="${productId}"].add-to-cart`).first().should('be.visible').click();
+    };
+
+    clickContinueShopping() {
+        cy.contains('button', 'Continue Shopping').click();
+    }
+
+    clickProceedToCheckout() {
+        cy.get('a.btn.btn-default.check_out').click();
+    }
+
+    reviewOrder(productId) {
+        cy.get(`a[href="/product_details/${productId}"]`).should('be.visible');
+    }
+
+    setComment(comment) {
+        cy.get('textarea[name="message"]').type(comment);
+    }
+
+    clickPlaceOrder() {
+        cy.get('a.btn.btn-default.check_out').click();
+    }
+
+    fillPaymentForm() {
+        cy.get('input[data-qa=name-on-card]').type(faker.person.fullName());
+        cy.get('input[data-qa=card-number]').type(faker.finance.creditCardNumber());
+        cy.get('input[data-qa=cvc]').type(faker.finance.creditCardCVV());
+        cy.get('input[data-qa=expiry-month]').type(faker.date.future().getMonth() + 1);
+        cy.get('input[data-qa=expiry-year]').type(faker.date.future().getFullYear());
+    }
+
+    clickPayAndConfirmOrder() {
+        cy.get('[data-qa="pay-button"]').click();
+    }
+    
+    checkout(productsId) {
+        this.clickProceedToCheckout();
+        this.checkCheckoutPage();
+        productsId.forEach(productId => {
+            this.reviewOrder(productId);
+        });
+    }
+
+    checkOrderPlaced() {
+        cy.get('[data-qa="order-placed"]').should('be.visible');
+        cy.get('.col-sm-9 > p').should('contain', SUCCESS_MSG_ORDER_PLACED);
+    }
+
+    checkCartPage() {
+        cy.url().should('contain', 'cart');
+        cy.get('section#cart_items').should('contain', 'Shopping Cart');
+    }
+
+    checkPaymentPage() {
+        cy.url().should('include', '/payment');
+        cy.get('section#cart_items')
+            .should('contain', 'Payment')
+            .should('be.visible');
+    }
+
+    checkCheckoutPage() {
+        cy.url().should('include', '/checkout');
+        cy.get('section#cart_items')
+            .should('contain', 'Checkout')
+            .should('be.visible');
+    }
+}
+
+export default new Cart()
